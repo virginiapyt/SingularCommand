295751606b8345ba319e836b5c257979a1339294
Wirginia Tomczyk <virginiapyt@poczta.onet.pl>
2022-01-12 19:52:41 +0100

    Dodaj pliki projektów.

---
+++ SingularCommand/ViewModels/MessageViewModel.cs
@@ -0,0 +1,26 @@
+﻿using SingularCommand.ViewModels.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SingularCommand.ViewModels
+{
+    public class MessageViewModel
+    {
+        public string MessageText { get; set; }
+        public MessageCommand DisplayMessageCommand { get; private set; }
+
+        public MessageViewModel()
+        {
+            DisplayMessageCommand = new MessageCommand(DisplayMessage);
+        }
+
+        public void DisplayMessage()
+        {
+            MessageBox.Show(MessageText);
+        }
+    }
+}
